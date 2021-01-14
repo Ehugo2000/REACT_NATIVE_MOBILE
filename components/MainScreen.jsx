72efe017cee5f7ea560c5aea9dd1f43e35884688
Sanne <sannelindholm@hotmail.com>
2021-01-14 12:35:44 +0100

    Creating ArticeShow component, adding show function in Articles module, adding touchableOpacity in ArticleIndexDetails and adding SET_CURRENT_ARRTICLE action in rootReducer. Adding some basic styling.

--- components/MainScreen.jsx
+++ components/MainScreen.jsx
@@ -5,7 +5,7 @@ import { StyleSheet, View, FlatList, Text } from "react-native";
 import Articles from "../modules/Articles";
 import ArticleIndexDetails from "./ArticleIndexDetails";
 
-const MainScreen = () => {
+const MainScreen = ({navigation}) => {
   const { articles } = useSelector((state) => state);
   useEffect(() => {
     Articles.index();
@@ -16,7 +16,7 @@ const MainScreen = () => {
       <FlatList
         data={articles}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => <ArticleIndexDetails article={item} />}
+        renderItem={({ item }) => <ArticleIndexDetails navigation={navigation} article={item} />}
       />
     </View>
   );
